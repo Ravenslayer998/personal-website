8977e5bf9da0e5e959774bfedcdae2a9cb9c1952
Jack Ooi <67252332+Ravenslayer998@users.noreply.github.com>
2023-10-12 18:05:25 +0800

    modified:   src/components/LandingSection.jsx 	deleted:    src/images/IMG_6841.heic

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 import profilePic from '../images/IMG_2796.png';
-import  background from "../images/IMG_6841.heic";
 
 // Introduction message for Full Scereen Section
 const greeting = "Hello, I am Jack!";
@@ -10,26 +9,26 @@ const bio1 = "Aspiring Software Engineer";
 const bio2 = "Specialise in React, Python ";
 const bio3 ="Location: Malaysian"
 
-function BackdropFilters() {
-  const outerBoxStyles = {
-    boxSize: '250px',
-    p: '10',
-    img:{background},
-  }
-}
+// function BackdropFilters() {
+//   const outerBoxStyles = {
+//     boxSize: '250px',
+//     p: '10',
+//     img:{background},
+//   }
+// }
 
-//Create Box section for contrast
-const innerBoxStyles = {
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  textAlign: 'center',
-  boxSize: 'full',
-  color: 'white',
-  textShadow: '0 0 20px black',
-  fontWeight: 'bold',
-  fontSize: '20px',
-}
+// //Create Box section for contrast
+// const innerBoxStyles = {
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   textAlign: 'center',
+//   boxSize: 'full',
+//   color: 'white',
+//   textShadow: '0 0 20px black',
+//   fontWeight: 'bold',
+//   fontSize: '20px',
+// }
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -42,7 +41,7 @@ const LandingSection = () => (
   >
     <VStack spacing={16}>
       <VStack spacing={4}>
-        <Avatar src={profilePic} size='2lg' name="Jack Ooi" alt="Profile Picture of Creator"/>
+        <Avatar src={profilePic} size='2xl' name="Jack Ooi" alt="Profile Picture of Creator"/>
         <Heading size="lg" >{greeting}</Heading>
       </VStack>
       <VStack spacing={4}>
