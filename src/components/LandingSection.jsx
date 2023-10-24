b08d94507be75189a68cbf337d3f3362fe674fd1
Jack Ooi <67252332+Ravenslayer998@users.noreply.github.com>
2023-10-24 11:20:05 +0800

    modified:   src/components/LandingSection.jsx 	modified:   src/index.css

--- src/components/LandingSection.jsx
+++ src/components/LandingSection.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Avatar, Heading, VStack, HStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
-import profilePic from '../images/IMG_2796.png';
+import profilePic from "../images/IMG_2796.png";
 
 // Introduction message for Full Scereen Section
 const greeting = "Hello, I am Jack!";
 const bio1 = "Aspiring Software Engineer";
 const bio2 = "Specialise in React, Python ";
-const Location ="Location: Kuala Lumpur, Malaysian"
+const Location = "Location: Kuala Lumpur, Malaysian";
 
 // function for getting current age
 var currentage = new Date().getFullYear() - 1998;
@@ -16,28 +16,33 @@ var currentage = new Date().getFullYear() - 1998;
 // Use a combination of Avatar, Heading and VStack components.
 
 const LandingSection = () => (
-  <FullScreenSection 
+  <FullScreenSection
     justifyContent="center"
     alignItems="center"
     isDarkBackground
     backgroundColor="rgba(0,0,0,0.4)"
     backgroundImage
-  >    
-    <VStack spacing={8} paddingLeft="2em" paddingRight="2em">
-        <VStack spacing={4} padding={10}>
-          <img src={profilePic} name="Jack Ooi" alt="Profile Picture of Jack" className="avatar"/>
-          <Heading size="md">{greeting}</Heading>
-        </VStack>
+  >
+    <VStack spacing={8} paddingLeft="2rem" paddingRight="2rem">
+      <VStack spacing={4} padding={10}>
+        <img
+          src={profilePic}
+          name="Jack Ooi"
+          alt="Profile Picture of Jack"
+          className="avatar"
+        />
+        <Heading size="md">{greeting}</Heading>
+      </VStack>
 
-        <VStack spacing={4} paddingBottom={4}>
-          <Heading size="2xl">{bio1}</Heading>
-          <Heading size="xl">{bio2}</Heading>
-          <HStack spacing={4}>  
+      <VStack spacing={4} paddingBottom={4}>
+        <Heading size="2xl">{bio1}</Heading>
+        <Heading size="xl">{bio2}</Heading>
+        <HStack spacing={4}>
           <Heading size="sm">{Location}</Heading>
           <Heading size="sm">Age: {currentage}</Heading>
-          </HStack>
-        </VStack>
-    </VStack>    
+        </HStack>
+      </VStack>
+    </VStack>
   </FullScreenSection>
 );
 
