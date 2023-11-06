22aacf326b3d9686cce907d816e31d42c2468a83
Jack Ooi <67252332+Ravenslayer998@users.noreply.github.com>
2023-11-06 22:31:02 +0800

    modified:   src/components/Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,16 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
+
+// OnClick function for Car
 const Card = ({ title, description, imageSrc, url}) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
+  const handleClick = () => {
+    alert('Button clicked!');
+  }
+  
   return(
     <div >
       <Image src={imageSrc}/>
       <Heading>{title}</Heading>
       <Text>{description}</Text>
-      <a href={url}></a>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
