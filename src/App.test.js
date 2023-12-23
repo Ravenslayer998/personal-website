a1d63024178107c31936bfb7aac9b1c0efd3e951
Jack Ooi <jack.ooi@bp.com>
2023-12-23 11:01:25 +0800

    modified:   src/App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, I am Jack/i);
+  const linkElement = screen.getByText(/Hello, I am Jack/);
   expect(linkElement).toBeInTheDocument();
 });
